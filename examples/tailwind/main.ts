1b933daa4ed6c8e12f2ef851d7b4c38a1c65b030
brandonjoyce2 <brandonj@xternsoftware.com>
2026-01-03 20:16:17 -0500

    Include Tailwind in examples

---
+++ examples/tailwind/main.ts
@@ -0,0 +1,10 @@
+import "@acme/gsap-carousel/styles.css";
+import "./style.css";
+
+// @ts-ignore
+import { createCarousel } from "@acme/gsap-carousel";
+
+createCarousel("[data-carousel]", {
+  loop: true,
+  initialIndex: 0
+});
