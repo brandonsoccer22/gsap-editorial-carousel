65db62cdd5bf888c720c3e39011cbf7e9548d783
brandonjoyce2 <brandonj@xternsoftware.com>
2026-01-03 20:22:21 -0500

    Minor updates to basic example

--- examples/basic/main.ts
+++ examples/basic/main.ts
@@ -1,6 +1,7 @@
 import "@acme/gsap-carousel/styles.css";
 import "./style.css";
 
+// @ts-ignore
 import { createCarousel } from "@acme/gsap-carousel";
 
 const carousel = createCarousel("[data-carousel]", {
